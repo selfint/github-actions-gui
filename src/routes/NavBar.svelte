578911bcc876f9d995aa015838d03eb67dce4a6b
selfint <40059156+selfint@users.noreply.github.com>
2023-03-23 10:10:27 +0000

    use grid in navbar

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,15 +1,19 @@
 <nav>
-	Github Actions UI
-	<a class="button edit" href="/">Edit</a>
-	<a class="button export" href="/export">Export</a>
+	<div>Github Actions UI</div>
+	<div id="buttons">
+		<a class="button edit" href="/">Edit</a>
+		<a class="button export" href="/export">Export</a>
+	</div>
 </nav>
 
 <style>
 	nav {
 		background-color: #1e1e1e;
 		color: #ffffff;
-		display: flex;
-		height: 100%;
+		display: grid;
+		grid-template-areas: 'title' 'buttons';
+		grid-template-columns: 1fr 1fr;
+		padding: 0.5em;
 	}
 
 	.edit {
@@ -30,5 +34,9 @@
 		height: fit-content;
 		width: fit-content;
 		text-decoration: none;
+	}
+
+	#buttons {
+		justify-self: end;
 	}
 </style>
